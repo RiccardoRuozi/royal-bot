cab7323e820683f4ebb941d2f771d316ed5b1136
Stefano Pigozzi <ste.pigozzi@gmail.com>
2015-11-15 16:58:07 +0100

    /sbaaaaaaam

--- bot.py
+++ bot.py
@@ -9,41 +9,41 @@ rage = []
 print("Bot avviato!")
 while True:
     # Guarda il comando ricevuto.
-    msg = telegram.getUpdates()
+    msg = telegram.getupdates()
     cmd = msg['text'].split(' ', 1)
     sby = msg['chat']['id']
     if cmd[0].startswith('/ahnonlosoio'):
         print(str(sby) + ": /ahnonlosoio")
-        telegram.sendMessage("Ah, non lo so nemmeno io!", sby)
+        telegram.sendmessage("Ah, non lo so nemmeno io!", sby)
     elif cmd[0].startswith('/ehoh'):
         print(str(sby) + ": /ehoh")
-        telegram.sendMessage("Eh, oh. Sono cose che capitano.", sby)
+        telegram.sendmessage("Eh, oh. Sono cose che capitano.", sby)
     elif cmd[0].startswith('/start'):
         print(str(sby) + ": /start")
-        telegram.sendMessage('Ascolta, io mi starto quando mi pare. Anzi, quando Steffo ha voglia di aprirmi.', sby)
+        telegram.sendmessage('Ascolta, io mi starto quando mi pare. Anzi, quando Steffo ha voglia di aprirmi.', sby)
     elif cmd[0].startswith('/playing'):
         print(str(sby) + ": /playing")
         if len(cmd) >= 2:
             n = steam.getNumberOfCurrentPlayers(cmd[1])
             if n is None:
-                telegram.sendMessage(chr(9888) + " L'app specificata non esiste!", sby)
+                telegram.sendmessage(chr(9888) + " L'app specificata non esiste!", sby)
             else:
-                telegram.sendMessage('In questo momento, ' + str(n) + ' persone stanno giocando a [' + cmd[1] +
+                telegram.sendmessage('In questo momento, ' + str(n) + ' persone stanno giocando a [' + cmd[1] +
                                      '](https://steamdb.info/app/' + cmd[1] + '/graphs/)', sby)
         else:
-            telegram.sendMessage(chr(9888) + ' Non hai specificato un AppID!\nLa sintassi corretta è /playing <AppID>.',
+            telegram.sendmessage(chr(9888) + ' Non hai specificato un AppID!\nLa sintassi corretta è /playing <AppID>.',
                                  sby)
     elif cmd[0].startswith('/saldistim'):
         print(str(sby) + ": /saldistim")
-        if (len(cmd) >= 2):
-            telegram.sendMessage(
+        if len(cmd) >= 2:
+            telegram.sendmessage(
                 'Ricerca di offerte di ' +
                 '[' + cmd[1] + '](https://isthereanydeal.com/#/search:' + cmd[1].replace(' ', '%20') +
                 ";/scroll:%23gamelist) completata.", sby)
         else:
-            telegram.sendMessage(chr(9888) +
-                                ' Non hai specificato un gioco!' +
-                                '[Visualizza tutto](https://isthereanydeal.com/#/search:.;/scroll:%23gamelist).', sby)
+            telegram.sendmessage(chr(9888) +
+                                 ' Non hai specificato un gioco!' +
+                                 '[Visualizza tutto](https://isthereanydeal.com/#/search:.;/scroll:%23gamelist).', sby)
     elif cmd[0].startswith('/rage'):
         if len(rage) <= 0:
             # Elenco degli audio disponibili
@@ -57,4 +57,7 @@ while True:
             random.shuffle(rage)
         ragesend = rage.pop()
         print(str(sby) + ": /rage " + ragesend)
-        telegram.sendDocument(ragesend, sby)
+        telegram.senddocument(ragesend, sby)
+    elif cmd[0].startswith('/sbam'):
+        print(str(sby) + ": /sbam ")
+        telegram.sendaudio('BQADAgADTQIAAh8GgAGj0jKIrpTgvQI', sby)
--- steam.py
+++ steam.py
@@ -2,64 +2,84 @@
 import requests
 import filemanager
 
-##Per far funzionare questa libreria serve un file "steamapi.txt" contenente la Steam Api Key ottenibile a http://steamcommunity.com/dev/apikey
-steamtoken = filemanager.readFile('steamapi.txt')
+# Per far funzionare questa libreria serve un file "steamapi.txt" contenente la Steam Api Key ottenibile a
+# http://steamcommunity.com/dev/apikey
+steamtoken = filemanager.readfile('steamapi.txt')
+
 
 def getPlayerSummaries(steamid):
-	"""Ottieni i dati del profilo steam del giocatore di cui è stato specificato lo SteamID 32."""
-	#Parametri della richiesta
-	parametri = {
-		'key': steamtoken,
-		'steamids': steamid,
-	}
-	#Manda la richiesta ai server di Steam.
-	r = requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/", params=parametri).json()
-	return r['response']['players'][0]
+    """Ottieni i dati del profilo steam del giocatore di cui è stato specificato lo SteamID 32.
+    :param steamid: SteamID 32 del giocatore
+    """
+    # Parametri della richiesta
+    parametri = {
+        'key': steamtoken,
+        'steamids': steamid,
+    }
+    # Manda la richiesta ai server di Steam.
+    r = requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/", params=parametri).json()
+    return r['response']['players'][0]
+
 
 def getNumberOfCurrentPlayers(appid):
-	"""Ottieni il numero di giocatori che stanno giocando a un certo gioco."""
-	#Parametri della richiesta
-	parametri = {
-		'key': steamtoken,
-		'appid': appid,
-	}
-	#Manda la richiesta ai server di Steam.
-	r = requests.get("http://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v0001/", params=parametri).json()
-	if('player_count' in r['response']):
-		return r['response']['player_count']
-	else:
-		return None
-	
+    """Ottieni il numero di giocatori che stanno giocando a un certo gioco.
+    :param appid: ID Steam dell'applicazione
+    """
+    # Parametri della richiesta
+    parametri = {
+        'key': steamtoken,
+        'appid': appid,
+    }
+    # Manda la richiesta ai server di Steam.
+    r = requests.get("http://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v0001/",
+                     params=parametri).json()
+    if 'player_count' in r['response']:
+        return r['response']['player_count']
+    else:
+        return None
+
+
 def getPlayerAchievements(appid, steamid):
-	"""Ottieni gli achievement del giocatore e del gioco specificato."""
-	#Parametri della richiesta
-	parametri = {
-		'key': steamtoken,
-		'steamid': steamid,
-		'appid': appid,
-		'l': 'IT' #Mettendo questo vengono in inglese...? What.
-	}
-	#Manda la richiesta ai server di Steam.
-	r = requests.get("http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/", params=parametri).json()
-	return r['playerstats']
+    """Ottieni gli achievement del giocatore e del gioco specificato.
+    :param appid: ID dell'applicazione
+    :param steamid: SteamID 32 del giocatore
+    """
+    # Parametri della richiesta
+    parametri = {
+        'key': steamtoken,
+        'steamid': steamid,
+        'appid': appid,
+        'l': 'IT'  # Mettendo questo vengono in inglese...? What.
+    }
+    # Manda la richiesta ai server di Steam.
+    r = requests.get("http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/",
+                     params=parametri).json()
+    return r['playerstats']
+
 
 def getSteamLevel(steamid):
-	"""Ottieni il livello del profilo di Steam."""
-	parametri = {
-		'key': steamtoken,
-		'steamid': steamid,
-	}
-	#Manda la richiesta ai server di Steam.
-	r = requests.get("http://api.steampowered.com/IPlayerService/GetSteamLevel/v0001/", params=parametri).json()
-	return r['response']['player_level']
+    """Ottieni il livello del profilo di Steam.
+    :param steamid: SteamID 32 del giocatore
+    """
+    parametri = {
+        'key': steamtoken,
+        'steamid': steamid,
+    }
+    # Manda la richiesta ai server di Steam.
+    r = requests.get("http://api.steampowered.com/IPlayerService/GetSteamLevel/v0001/", params=parametri).json()
+    return r['response']['player_level']
+
 
 def isPlayingSharedGame(appid, steamid):
-	"""Guarda se il gioco a cui sta giocando qualcuno è condiviso.\nAh, Steam vuole sapere l'ID del gioco a cui sta giocando, quindi mettetecelo."""
-	parametri = {
-		'key': steamtoken,
-		'steamid': steamid,
-		'appid_playing': appid,
-	}
-	#Manda la richiesta ai server di Steam.
-	r = requests.get("http://api.steampowered.com/IPlayerService/IsPlayingSharedGame/v0001/", params=parametri).json()
-	return r #Non posso provare il comando; cambiare quando possibile?
+    """Guarda se il gioco a cui sta giocando qualcuno è condiviso.
+    Ah, Steam vuole sapere l'ID del gioco a cui sta giocando, quindi mettetecelo.
+    :param appid: ID dell'applicazione
+    :param steamid: SteamID 32 del giocatore"""
+    parametri = {
+        'key': steamtoken,
+        'steamid': steamid,
+        'appid_playing': appid,
+    }
+    # Manda la richiesta ai server di Steam.
+    r = requests.get("http://api.steampowered.com/IPlayerService/IsPlayingSharedGame/v0001/", params=parametri).json()
+    return r  # Non posso provare il comando; cambiare quando possibile?
