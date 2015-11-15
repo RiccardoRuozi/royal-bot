cd6f0202253a4ba9d07d629abb8cc5b8f9192ae5
Stefano Pigozzi <ste.pigozzi@gmail.com>
2015-11-15 16:13:53 +0100

    1 nuovo audio

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ while True:
         telegram.sendMessage("Eh, oh. Sono cose che capitano.", sby)
     elif cmd[0].startswith('/start'):
         print(str(sby) + ": /start")
-        telegram.sendMessage("Ascolta, io mi starto quando mi pare. Anzi, quando Steffo ha voglia di aprirmi.", sby)
+        telegram.sendMessage('Ascolta, io mi starto quando mi pare. Anzi, quando Steffo ha voglia di aprirmi.', sby)
     elif cmd[0].startswith('/playing'):
         print(str(sby) + ": /playing")
         if len(cmd) >= 2:
@@ -28,22 +28,22 @@ while True:
             if n is None:
                 telegram.sendMessage(chr(9888) + " L'app specificata non esiste!", sby)
             else:
-                telegram.sendMessage("In questo momento, " + str(n) + " persone stanno giocando a [" + cmd[
-                    1] + "](https://steamdb.info/app/" + cmd[1] + "/graphs/)", sby)
+                telegram.sendMessage('In questo momento, ' + str(n) + ' persone stanno giocando a [' + cmd[1] +
+                                     '](https://steamdb.info/app/' + cmd[1] + '/graphs/)', sby)
         else:
-            telegram.sendMessage(chr(9888) + " Non hai specificato un AppID!\nLa sintassi corretta è /playing <AppID>.",
+            telegram.sendMessage(chr(9888) + ' Non hai specificato un AppID!\nLa sintassi corretta è /playing <AppID>.',
                                  sby)
     elif cmd[0].startswith('/saldistim'):
         print(str(sby) + ": /saldistim")
         if (len(cmd) >= 2):
             telegram.sendMessage(
-                "Ricerca di offerte di [" + cmd[1] + "](https://isthereanydeal.com/#/search:" + cmd[1].replace(' ',
-                                                                                                               '%20') + ';/scroll:%23gamelist) completata.',
-                sby)
+                'Ricerca di offerte di ' +
+                '[' + cmd[1] + '](https://isthereanydeal.com/#/search:' + cmd[1].replace(' ', '%20') +
+                ";/scroll:%23gamelist) completata.", sby)
         else:
-            telegram.sendMessage(chr(
-                9888) + " Non hai specificato un gioco! [Visualizza tutte le offerte](https://isthereanydeal.com/#/search:.;/scroll:%23gamelist).",
-                                 sby)
+            telegram.sendMessage(chr(9888) +
+                                ' Non hai specificato un gioco!' +
+                                '[Visualizza tutto](https://isthereanydeal.com/#/search:.;/scroll:%23gamelist).', sby)
     elif cmd[0].startswith('/rage'):
         if len(rage) <= 0:
             # Elenco degli audio disponibili
@@ -53,7 +53,7 @@ while True:
                     'BQADAgAD5gEAAh8GgAGvUTJ9meZixwI', 'BQADAgAD5wEAAh8GgAHJSoUnCr9WxwI',
                     'BQADAgAD6QEAAh8GgAExL8N1AWkDjgI', 'BQADAgAD6wEAAh8GgAFtkzazUqUEtwI',
                     'BQADAgAD9AEAAh8GgAE427GcA8FCqQI', 'BQADAgADMgIAAh8GgAEpusE7OCOXYgI',
-                    'BQADAgADMwIAAh8GgAFffavzkvOkKAI']
+                    'BQADAgADMwIAAh8GgAFffavzkvOkKAI', 'BQADAgADTAIAAh8GgAEgantYpHT5IwI']
             random.shuffle(rage)
         ragesend = rage.pop()
         print(str(sby) + ": /rage " + ragesend)
