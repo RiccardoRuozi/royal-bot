f6eb26e07ba28481bb043fa01c5fc0257df89c67
Stefano Pigozzi <ste.pigozzi@gmail.com>
2015-12-20 18:41:28 +0100

    /hs coming soon

---
+++ hearthstone.py
@@ -0,0 +1,6 @@
+import filemanager
+import requests
+
+# Per far funzionare questa libreria serve un file "hearthstoneapi.txt" con l'API key ricevuta da mashape.com
+apikey = filemanager.readfile('hearthstoneapi.txt')
+
