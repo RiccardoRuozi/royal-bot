4376c6b12bf88d6106223356a5acb8248ab1e4d8
Stefano Pigozzi <ste.pigozzi@gmail.com>
2016-01-19 21:17:56 +0100

    E invece no

--- bot.py
+++ bot.py
@@ -134,8 +134,8 @@ while True:
                                          "*300*: " + r['count300'] + "\n"
                                          "*100*: " + r['count100'] + "\n"
                                          "*50*: " + r['count50'] + "\n"
-                                         "*Awesome*: " + r['countgeki'] + "\n"
-                                         "*Good*: " + r['countkatu'] + "\n"
+                                         "*Awesome*: " + r['countkatu'] + "\n"
+                                         "*Good*: " + r['countgeki'] + "\n"
                                          "*Miss*: " + r['countmiss'], sentin)
                 elif mode == 1:
                     telegram.sendmessage("*Taiko*\n"
