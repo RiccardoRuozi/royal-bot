905c5dc22e4d956d1424d7eaf0eccfe50d24bb1e
Stefano Pigozzi <ste.pigozzi@gmail.com>
2016-04-01 22:28:41 +0200

    Robe

--- mifia.py
+++ mifia.py
@@ -5,7 +5,7 @@ import telegram
 class Player:
     telegramid = int()
     username = str()
-    role = int()
+    role = 0 # 0 = normale, 1 = mifia
     alive = True
 
     def message(self, text):
@@ -27,7 +27,15 @@ class Game:
         """Manda un messaggio alla chat generale del gioco
         :param text: Testo del messaggio
         """
-        telegram.sendmessage(text, self.telegramid)
+        telegram.sendmessage(text, self.chat)
+
+    def mifiamessage(self, text):
+        """Manda un messaggio alla chat generale del gioco
+        :param text: Testo del messaggio
+        """
+        for player in self.players:
+            if player.role == 1:
+                telegram.sendmessage(text, player.telegramid)
 
     def displaystatus(self):
         """Visualizza lo stato attuale della partita"""
@@ -37,6 +45,19 @@ class Game:
                 tosend += "\U0001F480 "
             else:
                 tosend += "\U0001F636 "
+            tosend += player.username + "\n"
+        self.message(tosend)
+
+    def displayfullstatus(self):
+        """Visualizza lo stato attuale della partita (per admin?)"""
+        tosend = "Stato attuale del gioco: \n"
+        for player in self.players:
+            if not player.alive:
+                tosend += "_Morto_ "
+            elif player.role == 1:
+                tosend += "_Mifia_ "
+            else:
+                tosend += "_Civile_ "
             tosend += player.username + "\n"
         self.message(tosend)
 
