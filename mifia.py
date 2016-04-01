0eacbd4293bed800e6526b4aadfc9618091d3e45
Stefano Pigozzi <ste.pigozzi@gmail.com>
2016-04-01 22:08:08 +0200

    Aggiunto cose

--- mifia.py
+++ mifia.py
@@ -22,3 +22,26 @@ class Player:
 class Game:
     chat = int()
     players = list()
+
+    def message(self, text):
+        """Manda un messaggio alla chat generale del gioco
+        :param text: Testo del messaggio
+        """
+        telegram.sendmessage(text, self.telegramid)
+
+    def displaystatus(self):
+        """Visualizza lo stato attuale della partita"""
+        tosend = "Stato attuale del gioco: \n"
+        for player in self.players:
+            if not player.alive:
+                tosend += "\U0001F480 "
+            else:
+                tosend += "\U0001F636 "
+            tosend += player.username + "\n"
+        self.message(tosend)
+
+    def addplayer(self, player):
+        """Aggiungi un giocatore alla partita
+        :param player: Oggetto del giocatore da aggiungere
+        """
+        self.players.append(player)
