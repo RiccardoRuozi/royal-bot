29ea17b9b8c0e937d1adb093ddc29de832b56927
Stefano Pigozzi <ste.pigozzi@gmail.com>
2016-04-01 18:20:10 +0200

    Aggiunte funzioni al player

--- mifia.py
+++ mifia.py
@@ -1,10 +1,22 @@
 # -*- coding: utf-8 -*-
+import telegram
 
 
 class Player:
     telegramid = int()
     username = str()
     role = int()
+    alive = True
+
+    def message(self, text):
+        """Manda un messaggio al giocatore
+        :param text: Testo del messaggio
+        """
+        telegram.sendmessage(text, self.telegramid)
+
+    def kill(self):
+        """Uccidi il giocatore"""
+        self.alive = False
 
 
 class Game:
